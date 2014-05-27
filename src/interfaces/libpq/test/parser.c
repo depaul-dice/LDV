d6fb1afd2750c573d4c3595b9b39c7f4b7124943
Quan Pham <quanpt@gmail.com>
2014-05-27 10:07:18 -0500

    parser prints query tree

--- src/interfaces/libpq/test/parser.c
+++ src/interfaces/libpq/test/parser.c
@@ -4,33 +4,74 @@
  *  Created on: May 24, 2014
  *      Author: quanpt
  */
-
 #include "postgres.h"
-#include "nodes/pg_list.h"
-#include "parser/parser.h"
-
-/* Write a Node field */
-#define WRITE_NODE_FIELD(fldname) \
-	(appendStringInfo(str, " :" CppAsString(fldname) " "), \
-	 _outNode(str, node->fldname))
+#include <ctype.h>
+#include <lib/stringinfo.h>
+#include <nodes/nodes.h>
+#include <nodes/parsenodes.h>
+#include <nodes/pg_list.h>
+#include <parser/parser.h>
+#include <port.h>
+#include <stdio.h>
+#include <storage/ipc.h>
+#include <utils/memutils.h>
+#include <utils/palloc.h>
 
 /* Write the label for the node type */
 #define WRITE_NODE_TYPE(nodelabel) \
 	appendStringInfoString(str, nodelabel)
 
+/* Write a Node field */
+#define WRITE_NODE_FIELD(fldname) \
+	(appendStringInfo(str, "\n    :" CppAsString(fldname) " "), \
+	 _outNode(str, node->fldname))
+
+/* Write a character-string (possibly NULL) field */
+#define WRITE_STRING_FIELD(fldname) \
+	(appendStringInfo(str, "\n    :" CppAsString(fldname) " "), \
+	 _outToken(str, node->fldname))
+
+/* Write an enumerated-type field as an integer code */
+#define WRITE_ENUM_FIELD(fldname, enumtype) \
+	appendStringInfo(str, "\n    :" CppAsString(fldname) " %d", \
+					 (int) node->fldname)
+
 const char *progname;
 
-typedef struct InsertionInfo
-{
-	NodeTag		type;
-	Node       *relation;		/* relation to insert into */
-	List	   *cols;			/* optional: names of the target columns */
-	Node	   *selectStmt;		/* the source SELECT/VALUES, or NULL */
-	List	   *returningList;	/* list of expressions to return */
-} InsertionInfo; // get this from _copyInsertStmt
+/*
+ * _outToken
+ *	  Convert an ordinary string (eg, an identifier) into a form that
+ *	  will be decoded back to a plain token by read.c's functions.
+ *
+ *	  If a null or empty string is given, it is encoded as "<>".
+ */
+static void _outToken(StringInfo str, char *s) {
+	if (s == NULL || *s == '\0') {
+		appendStringInfo(str, "<>");
+		return;
+	}
+
+	/*
+	 * Look for characters or patterns that are treated specially by read.c
+	 * (either in pg_strtok() or in nodeRead()), and therefore need a
+	 * protective backslash.
+	 */
+	/* These characters only need to be quoted at the start of the string */
+	if (*s == '<' || *s == '\"' || isdigit((unsigned char ) *s)
+			|| ((*s == '+' || *s == '-')
+					&& (isdigit((unsigned char) s[1]) || s[1] == '.')))
+		appendStringInfoChar(str, '\\');
+	while (*s) {
+		/* These chars must be backslashed anywhere in the string */
+		if (*s == ' ' || *s == '\n' || *s == '\t' || *s == '(' || *s == ')'
+				|| *s == '{' || *s == '}' || *s == '\\')
+			appendStringInfoChar(str, '\\');
+		appendStringInfoChar(str, *s++);
+	}
+}
 
 static void
-_outInsertionInfo(StringInfo str, InsertionInfo *node) {
+_outInsertionInfo(StringInfo str, InsertStmt *node) {
 	WRITE_NODE_TYPE("INSERTTIONINFO");
 
 	WRITE_NODE_FIELD(relation);
@@ -39,12 +80,31 @@ _outInsertionInfo(StringInfo str, InsertionInfo *node) {
 	WRITE_NODE_FIELD(returningList);
 }
 
+static void
+_outCreateInfo(StringInfo str, CreateStmt *node) {
+	WRITE_NODE_TYPE("CREATEINFO");
+
+	WRITE_NODE_FIELD(relation);
+	WRITE_NODE_FIELD(tableElts);
+	WRITE_NODE_FIELD(inhRelations);
+	WRITE_NODE_FIELD(constraints);
+	WRITE_NODE_FIELD(options);
+	WRITE_ENUM_FIELD(oncommit, OnCommitAction);
+	WRITE_STRING_FIELD(tablespacename);
+}
+
 void
 ptuOutNode(StringInfo str, void *obj) {
 	switch(nodeTag(obj)) {
 	case T_InsertStmt:
 		_outInsertionInfo(str, obj);
 		break;
+	case T_CreateStmt:
+		_outCreateInfo(str, obj);
+		break;
+	default:
+		_outNode(str, obj);
+//		elog(ERROR, "unrecognized node type: %d", (int) nodeTag(obj));
 	}
 }
 
@@ -58,40 +118,57 @@ _nodeToString(void *obj) {
 	return str.data;
 }
 
-void print_tree(List* raw_parsetree_list, int depth) {
-	ListCell *list_item;
-	printf("%*s" "%d %d\n", depth * 4, " ", raw_parsetree_list->type, raw_parsetree_list->length);
-	foreach(list_item, raw_parsetree_list) {
-		Node	   *stmt = (Node *) lfirst(list_item);
+char *stringToSql(char *str) {
+	StringInfoData sql;
+	initStringInfo(&sql);
+	return sql.data;
+}
+
+void print_tree(List* raw_parsetree_list) {
+	ListCell *list_item;foreach(list_item, raw_parsetree_list) {
+		Node *stmt = (Node *) lfirst(list_item);
 		char *msg = _nodeToString(stmt);
-		printf("%s\n", msg);
+		char *sql = stringToSql(msg);
+		printf("%s\n--> %s\n", msg, sql);
 		pfree(msg);
+		pfree(sql);
 	}
+}
+
+void print_tree_sql(char* sql) {
+	List *parsetree;
+
+	printf("\n\n========\nSQL: '%s'\n", sql);
+
+	parsetree = raw_parser(sql);
+	if (parsetree == NIL) {
+		printf("Error parsing '%s'\n", sql);
+		return;
+	}
+	print_tree(parsetree);
+	list_free_deep(parsetree);
 }
 
 int main(int argc, char** argv) {
 	char *sql = "INSERT into weather values(0, \"cool\");";
-	List *parsetree;
+
 
 	progname = get_progname(argv[0]);
 
-	argv = save_ps_display_args(argc, argv);
-	set_pglocale_pgservice(argv[0], "postgres");
-
 	if (argc == 2)
 		sql = argv[1];
-	printf("SQL: '%s'\n", sql);
 
 	MemoryContextInit();
-	parsetree = raw_parser(sql);
-	if (parsetree == NIL) {
-		printf("Error\n");
-		return -1;
-	}
-	print_tree(parsetree, 1);
 
-	list_free_deep(parsetree);
-	proc_exit();
+	print_tree_sql(sql);
+
+	print_tree_sql("CREATE TABLE tbl1 (id INTEGER, value INTEGER)");
+	print_tree_sql("INSERT INTO tbl1 values(79, 1)");
+	print_tree_sql("INSERT INTO tbl1 values(49, 24)");
+	print_tree_sql("SELECT sum(value) FROM tbl1 WHERE value < 50");
+	print_tree_sql("INSERT INTO distributors (did, dname) VALUES (DEFAULT, 'XYZ Widgets')");
+
+	proc_exit(0);
 	return 0;
 }
 
