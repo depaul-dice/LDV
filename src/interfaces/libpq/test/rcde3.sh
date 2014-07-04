f98e37f397169978a8b4d40dece44c6ed421b61a
Quan Pham <quanpt@gmail.com>
2014-07-04 18:13:24 -0500

    make runable before changing to bigtest

--- src/interfaces/libpq/test/rcde3.sh
+++ src/interfaces/libpq/test/rcde3.sh
@@ -20,19 +20,18 @@ fi
 #~ rm -rf data >/dev/null 2>&1 
 #~ bin/initdb -D data >/dev/null 2>&1 
 #~ chmod -R go-rx data
+#~ bin/pg_ctl start -D data -l logfile > /dev/null 2>&1
 #~ cd $oldpath
 
-####
-#~ remember to add to cde.options
-#~ ignore_environment_var=PTU_DB_MODE
-#~ ignore_environment_var=PTU_DBSESSION_ID
-#~ ignore_environment_var=PTU_DB_REPLAY
-
 ### start
-export PTU_DB_REPLAY=/dblog.txt
-export PTU_DB_MODE=22
-#~ ./exp.sh
+#~ export PTU_DB_REPLAY=dblog.txt
+#~ cp `ls *.dblog | tail -n 1` dblog.txt
 
-# real re-run
-cd cde-package
-sh cde.log
+export PTU_DB_REPLAY=`ls *.dblog | tail -n 1`
+export PTU_DB_MODE=32
+cp $PTU_DB_REPLAY cde-package/cde-root/$oldpath
+echo "Rerun query without actual postgresql server"
+cd cde-package/cde-root/$oldpath
+$oldpath/cde-package/cde-exec ./query "host=localhost dbname=single" 0
+
+
