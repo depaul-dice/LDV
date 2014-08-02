ff7cf8dcddad62348371ec34d3df8052067d9c76
Quan Pham <quanpt@gmail.com>
2014-08-01 23:04:52 -0500

    case 3 works

---
+++ src/interfaces/libpq/tpch/normal.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+### prepare clean db and newest binary
+cp single query
+oldpath=`pwd`
+if [ "$PERM" = "" ]
+then
+  if [ `hostname` = "qpc" ]
+  then
+    PERM=/home/quanpt/myapps/perm
+  else
+    PERM=/var/tmp/quanpt/froot/perm
+  fi
+fi
+
+# prepare original database
+cd $PERM
+killall psql 2>/dev/null
+bin/pg_ctl stop -D data 2>/dev/null
+killall postgres
+rm -rf data 2>/dev/null
+tar xzf $oldpath/data.tpch.tgz
+cd $oldpath
+
+### start
+export PTU_DBSESSION_ID=0
+export PTU_DB_MODE=0
+export LD_LIBRARY_PATH=../
+unset PTU_DB_REPLAY
+rm *.dblog 2>/dev/null
+
+N=`grep real time.exp.txt | wc -l`
+./exp.sh
+grep real time.exp.txt | tail -n +$N
